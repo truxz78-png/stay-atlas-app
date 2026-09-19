bd1733bd60479c8ec97d2792196630c67d78c8e4
truxz78-png <truxz78@gmail.com>
2026-09-19 00:34:37 -0400

    Implement MainActivity with WebView to load URL

---
+++ androidApp/app/src/main/java/com/stayatlas/app/MainActivity.java
@@ -0,0 +1,38 @@
+package com.stayatlas.app;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebSettings;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+    private static final String SITE_URL = "https://truxz78-png.github.io/stay-atlas-app/";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(SITE_URL);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
